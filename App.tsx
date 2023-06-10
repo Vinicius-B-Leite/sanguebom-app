ecab51e74fa31d6ae710d9e57ef34f546aff8e6f
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-06-10 11:52:53 -0300

    feature: configurando o one signal

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect, useLayoutEffect } from 'react';
-import { View } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import { darkMode } from './src/theme/darkMode';
+
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { RootState, store } from './src/feature/store';
-import Routes from './src/routes';
-import { setUser } from './src/feature/user/userSlicer';
-import { lightMode } from './src/theme/lightMode';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { changeTheme } from './src/feature/theme/themeSlicer';
+
 import Index from './src';
+import OneSignal from 'react-native-onesignal';
 
 
+
+OneSignal.setAppId('6df54534-d625-4068-a0bb-efa0e5917998')
 const App: React.FC = () => {
 
-  
+
   return (
     <Provider store={store}>
       <Index />
--- tasks.md
+++ tasks.md
@@ -1,7 +1,7 @@
 
 task atual: 
     dropdown
-
+6df54534-d625-4068-a0bb-efa0e5917998
 
 
     melhorar o UI do alerta no mapa
